35d67fa78f615be50ff04f1becd4cc127d46a7ae
Rikishiyayo <rikishiyayo@gmail.com>
2015-11-25 23:59:15 +0100

    Adding the core script file to the repository

---
+++ bussinesLogic/DirectoryAndFileReader.py
@@ -0,0 +1,113 @@
+import os
+from config import EXP_DIRECTORY
+
+
+# reads a directory for a current logged in user and return his experiments
+#
+# user_id - id of a user which matches a directory on a server where this user has his experiments saved
+#
+# returns an array of dictionaries - dictionary has 4 keys: title, date, status and progress
+def get_experiments(user_id):
+    experiments = []
+
+    for item in os.listdir(os.path.join(EXP_DIRECTORY, str(user_id))):
+        info = { "exp_guid": item, "user_id": user_id }
+        read_file(os.path.join(EXP_DIRECTORY, str(user_id), item, "params.txt"), info, ["title", "date"])
+        read_file(os.path.join(EXP_DIRECTORY, str(user_id), item, "result.dat"), info, ["status", "progress"])
+        experiments.append(info)
+
+    return experiments
+    
+    
+#
+#
+#
+#
+#
+def get_computed_values_for_models(user_id, exp_guid):
+    directory = os.path.join(EXP_DIRECTORY, user_id, exp_guid)
+    models = []
+    
+    for item in os.listdir(os.path.join(directory, "DemoValues")):
+        points = []
+        # Open the text file for reading
+        file = open(os.path.join(directory, "DemoValues", item))
+        lines = file.readlines()
+        
+        for line in lines[2:]:
+            values_in_line = line.split()
+            points.append({ "q_value" : float(values_in_line[0].strip()),
+                            "intensity" : float(values_in_line[1].strip())
+                          })
+            
+        models.append({ "model" : points})
+    
+    return models
+
+
+#
+#
+#
+#
+#
+def get_weights(user_id, exp_guid):
+    filepath = os.path.join(EXP_DIRECTORY, user_id, exp_guid, "result.dat")
+    weights = []    
+    # Open the text file for reading
+    file = open(filepath)    
+    lines = file.readlines()
+    values_in_line = lines[2].split()
+    
+    for value in values_in_line[1:]:
+        weights.append(value.replace(",", "").strip())
+        
+    file.close()   
+    return weights
+
+
+#
+#
+def get_experiment_result_data(user_id, exp_guid):
+    result_file_path = os.path.join(EXP_DIRECTORY, user_id, exp_guid, "result.dat")
+
+    info = {}
+    read_file(result_file_path, info, ["status", "progress"])
+
+    return info
+    
+
+#
+#
+def get_experiment_parameters(user_id, exp_guid):
+    parameters_file_path = os.path.join(EXP_DIRECTORY, user_id, exp_guid, "params.txt")
+
+    info = {}
+    read_file(parameters_file_path, info, ["title", "date", "comment", "steps", "sync", "alpha", "beta", "gama"])
+
+    return info
+
+
+# reads a file and adds key:value pairs into a dictionary - finds a key string in a file
+# and gets its value(a substring after ':' char)
+#
+# filePath - path of the file to read
+# dictionary - reference to a dictionary to which new key:value pairs will be appended
+# keys - a list of strings to find in a file. Each string represents a key in dictionary
+def read_file(file_path, dictionary, keys):
+    file = open(file_path)
+    lines = file.readlines()
+    for key in keys:
+        for line in lines:
+            if key in line:
+                dictionary[key] = line.split(':')[1].strip()
+
+    file.close()
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ bussinesLogic/DirectoryAndFileWriter.py
@@ -0,0 +1,82 @@
+import os
+import time
+import shutil
+import uuid
+from werkzeug.utils import secure_filename
+from config import APP_ROOT, EXP_DIRECTORY
+
+
+# creates an experiment - creates a directory, uploads and creates required files
+# form - a reference to a form with data required to create a new experiment
+def create_experiment(form, user_id):
+    exp_identificator = str(uuid.uuid4())
+
+    # create a directory for this experiment
+    os.chdir(os.path.join(EXP_DIRECTORY, user_id))
+    os.mkdir(exp_identificator)
+
+    create_params_file(os.path.join(EXP_DIRECTORY, user_id, exp_identificator), form)
+    create_result_file(os.path.join(EXP_DIRECTORY, user_id, exp_identificator))
+    upload_file(form.models.data, os.path.join(EXP_DIRECTORY, user_id, exp_identificator))
+    upload_file(form.expData.data, os.path.join(EXP_DIRECTORY, user_id, exp_identificator))
+
+
+# uploads a file to a specified directory
+# file - a reference to a file to upload
+# path - a path to a directory where the  file will be uploaded
+def upload_file(file, path):
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(path, filename))
+
+
+# creates file params.txt in specified directory with data from form
+# path - a path to a directory where the params.txt file will be created
+# form - a reference to a form with data which will be written in params.txt
+def create_params_file(path, form):
+    params = open(os.path.join(path, "params.txt"), "a+")
+
+    params.write(form.title.name + ':' + form.title.data + '\n')
+    params.write("date:" + time.strftime("%d/%m/%Y") + '\n')
+    params.write(form.description.name + ':' + form.description.data + '\n')
+    params.write(form.calcSteps.name + ':' + str(form.calcSteps.data) + '\n')
+    params.write(form.stepsBetweenSync.name + ':' + str(form.stepsBetweenSync.data) + '\n')
+    params.write(form.alpha.name + ':' + str(form.alpha.data) + '\n')
+    params.write(form.beta.name + ':' + str(form.beta.data) + '\n')
+    params.write(form.gama.name + ':' + str(form.gama.data))
+
+    params.close()
+
+
+# creates file result.dat in specified directory
+# path - a path to a directory where the result.dat file will be created
+def create_result_file(path):
+    result = open(os.path.join(path, "result.dat"), "a+")
+    result.write("status:accepted\nprogress:40%")
+    result.close()
+    
+
+# gets a file with model to display and moves it to a directory on a server for Jsmol to read and for user to download
+# gets a file with experiment data and moves it to a directory on a serverfor user to download
+# user_id - an id of a user that represents a directory where experiment data are stored
+# exp_guid - an identificator of a experiment
+def get_model_data(user_id, exp_guid):
+    src_file_path_models = os.path.join(EXP_DIRECTORY, user_id, exp_guid, "final.pdb")
+    dst_file_path_models = os.path.join(APP_ROOT, "app/static/uploads/final.pdb")
+    shutil.copyfile(src_file_path_models, dst_file_path_models)
+
+    src_file_path_exp_data = os.path.join(EXP_DIRECTORY, user_id, exp_guid, "exp_data.dat")
+    dst_file_path_exp_data = os.path.join(APP_ROOT, "app/static/uploads/exp_data.dat")
+    shutil.copyfile(src_file_path_exp_data, dst_file_path_exp_data)
+
+
+def create_user_directory(user_id):
+    # create a directory for a new user
+    os.chdir(os.path.join(EXP_DIRECTORY))
+    os.mkdir(str(user_id))
+
+
+
+
+
+
+
